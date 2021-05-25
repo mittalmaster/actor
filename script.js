de80f46b857b17717fe456ff086346ea285a7dcf
Ujjwal Mittal <50482003+mittalmaster@users.noreply.github.com>
2021-05-25 14:22:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+function display1()
+{
+    var x = document.getElementById("Madras Cafe");
+    x.style.display= "block";
+        
+}
+
+
+function display2()
+{
+    var x = document.getElementById("batla");
+    x.style.display= "block";
+        
+}
+
+
+function display3()
+{
+    var x = document.getElementById("no smoking");
+    x.style.display= "block";
+        
+}
+
+
+function display4()
+{
+    var x = document.getElementById("force");
+    x.style.display= "block";
+        
+}
+
+
+function display5()
+{
+    var x = document.getElementById("Parmanu");
+    x.style.display= "block";
+        
+}
+
+
